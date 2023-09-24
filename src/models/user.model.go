94db0f7a614c96a619d926c1655246616c807d18
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-09-24 21:00:04 +0100

    feat: Added default values to UserVerified and Currency

--- src/models/user.model.go
+++ src/models/user.model.go
@@ -6,9 +6,9 @@ type User struct {
 	gorm.Model
 	DisplayName     string `gorm:"type:varchar(100);not null"`
 	Email           string `gorm:"type:varchar(100);not null"`
-	UserVerified    bool   `gorm:"type:boolean;not null"`
+	UserVerified    bool   `gorm:"type:boolean;not null;default:false"`
 	VerifyUserToken string `gorm:"type:varchar(100);not null"`
-	Currency        string `gorm:"type:varchar(100);not null"`
+	Currency        string `gorm:"type:varchar(100);not null;default:'EUR'"`
 
 	Password Password  `gorm:"foreignkey:UserID"`
 	Sessions []Session `gorm:"foreignkey:UserID"`
