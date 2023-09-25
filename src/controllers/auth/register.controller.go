fe04aae16c3c73255352f3ca27d9573594d19f34
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-09-25 14:59:12 +0100

    docs: Added missing comments

--- src/controllers/auth/register.controller.go
+++ src/controllers/auth/register.controller.go
@@ -10,24 +10,33 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// DB_ERROR is a constant that holds the error message for database errors (used multiple times)
 const DB_ERROR = "an error occured with our database"
 
+// Register creates a new user and password in the database, generates tokens, hashes the password, and sends a verification email.
+// It receives an echo context and a BodyData struct containing the email, display name, and password of the user to be created.
+// It returns a ResponseData struct containing the user's ID, email, display name, currency, user verification status, and creation date, and an error.
 func Register(c echo.Context, bodyData schemas.BodyData) (schemas.ResponseData, error) {
 	var responseData schemas.ResponseData
 	var err error
 
+	// Generate tokens
 	verifyUserToken, verifyUserTokenErr := utils.GenerateToken()
 	resetPasswordToken, resetPasswordTokenErr := utils.GenerateToken()
 
+	// Hash password
 	hashedPassword, hashedPasswordErr := utils.HashPassword(bodyData.Password)
 
+	// Check if there was an error while generating tokens or hashing the password
 	if verifyUserTokenErr != nil || resetPasswordTokenErr != nil || hashedPasswordErr != nil {
 		err = errors.New("an error occured while creating the user")
 		return responseData, err
 	}
 
+	// Get the database instance
 	db := models.DB()
 
+	// Create the user and password in a single transaction
 	tx := db.Begin()
 	if tx.Error != nil {
 		err = errors.New(DB_ERROR)
@@ -58,8 +67,9 @@ func Register(c echo.Context, bodyData schemas.BodyData) (schemas.ResponseData, 
 		return responseData, err
 	}
 
-	tx.Commit()
+	tx.Commit() // Commit the transaction if all operations are successful
 
+	// Send the verification email
 	services.SendEmail(
 		bodyData.Email,
 		bodyData.DisplayName,
