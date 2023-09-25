646b551793778816378f0e50eb7a5cd4347ef48b
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-09-25 14:26:14 +0100

    feat: Mailjet lib to send emails

--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 	github.com/labstack/echo/v4 v4.11.1 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/mailjet/mailjet-apiv3-go/v3 v3.2.0 // indirect
+	github.com/mailjet/mailjet-apiv3-go/v4 v4.0.1 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
---
+++ src/services/sendEmail.service.go
@@ -0,0 +1,39 @@
+package services
+
+import (
+	"easy-wallet-be/src/utils"
+	"fmt"
+	"log"
+
+	"github.com/mailjet/mailjet-apiv3-go/v4"
+)
+
+func SendEmail(email string, name string, subject string, htmlPart string) {
+	mailjetClient := mailjet.NewMailjetClient(
+		utils.GetEnv("MAILJET_PUBLIC_KEY"),
+		utils.GetEnv("MAILJET_SECRET_KEY"),
+	)
+
+	messagesInfo := []mailjet.InfoMessagesV31{
+		{
+			From: &mailjet.RecipientV31{
+				Email: "tiggy.ribeiro@gmail.com",
+				Name:  "Easy Wallet Team",
+			},
+			To: &mailjet.RecipientsV31{
+				mailjet.RecipientV31{
+					Email: email,
+					Name:  name,
+				},
+			},
+			Subject:  subject,
+			HTMLPart: htmlPart,
+		},
+	}
+	messages := mailjet.MessagesV31{Info: messagesInfo}
+	res, err := mailjetClient.SendMailV31(&messages)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Data: %+v\n", res)
+}
