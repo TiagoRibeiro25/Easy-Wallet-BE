b9af1b722f831341a1523c3bc85a4b91bd74f0c5
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-09-25 14:36:10 +0100

    feat: Add email verification token for sign-up confirmation
    Unfinished

--- src/controllers/auth/register.controller.go
+++ src/controllers/auth/register.controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	schemas "easy-wallet-be/src/data/schemas/auth/register"
 	"easy-wallet-be/src/models"
+	"easy-wallet-be/src/services"
 	"easy-wallet-be/src/utils"
 	"net/http"
 
@@ -15,6 +16,7 @@ const DB_ERROR = "An error occured with our database"
 // Register creates a new user and password in the database, and returns a response with the user's information.
 // It receives a context and a BodyData struct containing the user's email, display name and password.
 // It generates a verification token and a reset password token, hashes the password, and creates the user and password in a single transaction.
+// It sends a verification email to the user's email address.
 // It returns an error if there was an error while generating tokens or hashing the password, or if there was an error while creating the user or password in the database.
 func Register(c echo.Context, bodyData schemas.BodyData) error {
 	// Generate tokens
@@ -54,7 +56,6 @@ func Register(c echo.Context, bodyData schemas.BodyData) error {
 		VerifyUserToken: verifyUserToken,
 	}
 
-	// Create both user and password in a single transaction
 	if err := tx.Create(&user).Error; err != nil {
 		tx.Rollback()
 		return utils.HandleResponse(
@@ -83,7 +84,14 @@ func Register(c echo.Context, bodyData schemas.BodyData) error {
 
 	tx.Commit() // Commit the transaction if all operations are successful
 
-	//TODO: Send an email to the user with the verification token
+	// Send the verification email
+	services.SendEmail(
+		bodyData.Email,
+		bodyData.DisplayName,
+		"Welcome to Easy Wallet",
+		"<h4>Verify User Token</h4><p>"+verifyUserToken+"</p>",
+	)
+
 	//TODO: Add a cron job to delete the token after 15 minutes and create a new one
 	//TODO: Add a cron job to delete the user after 24 hours if the user is not verified
 
