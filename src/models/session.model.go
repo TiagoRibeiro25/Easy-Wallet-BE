f957f2ae997a5efc7926eaa095f07298d01518ed
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-09-26 15:07:17 +0100

    feat: Added ExpiredAt to the Session model

--- src/models/session.model.go
+++ src/models/session.model.go
@@ -1,10 +1,15 @@
 package models
 
-import "github.com/jinzhu/gorm"
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+)
 
 type Session struct {
 	gorm.Model
-	SessionID           string `gorm:"type:varchar(100);unique_index;not null"`
-	ConfirmSessionToken string `gorm:"type:varchar(100);not null"`
-	UserID              uint   `gorm:"unique_index;not null"`
+	SessionID           string    `gorm:"type:varchar(100);unique_index;not null"`
+	ConfirmSessionToken string    `gorm:"type:varchar(100);not null"`
+	UserID              uint      `gorm:"unique_index;not null"`
+	ExpiresAt           time.Time `gorm:"default:current_timestamp"`
 }
