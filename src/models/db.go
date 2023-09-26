71a045a07e78973974e943cb348d2d53287b7815
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-09-26 18:45:00 +0100

    refactor: Commented migrate function

--- src/models/db.go
+++ src/models/db.go
@@ -54,22 +54,22 @@ func SetupDatabase() (*gorm.DB, error) {
 
 // `setUpModels` sets up the database models by creating the necessary tables and relationships.
 // It also retrieves the first user and password from the database and preloads their associated models.
-func setUpModels() {
-	color.Cyan("Setting up models...")
+// func setUpModels() {
+// 	color.Cyan("Setting up models...")
 
-	// AutoMigrate the models to create the necessary tables
-	err := db.AutoMigrate(
-		&User{},
-		&Password{},
-		&Session{},
-		&Income{},
-		&Expense{},
-		&Category{},
-	).Error
-	utils.HandleError(err, "Error migrating models", true)
+// 	// AutoMigrate the models to create the necessary tables
+// 	err := db.AutoMigrate(
+// 		&User{},
+// 		&Password{},
+// 		&Session{},
+// 		&Income{},
+// 		&Expense{},
+// 		&Category{},
+// 	).Error
+// 	utils.HandleError(err, "Error migrating models", true)
 
-	color.Green("Successfully set up models")
-}
+// 	color.Green("Successfully set up models")
+// }
 
 // `DB` returns the shared database connection
 func DB() *gorm.DB {
