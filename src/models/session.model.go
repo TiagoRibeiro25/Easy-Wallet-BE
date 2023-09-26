7fd26674976cfe0c71753ee37703ea4ad89196d1
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-09-26 17:06:57 +0100

    refactor: Removed ConfirmSessionToken from session model

--- src/models/session.model.go
+++ src/models/session.model.go
@@ -8,8 +8,7 @@ import (
 
 type Session struct {
 	gorm.Model
-	SessionID           string    `gorm:"type:varchar(100);unique_index;not null"`
-	ConfirmSessionToken string    `gorm:"type:varchar(100);not null"`
-	UserID              uint      `gorm:"unique_index;not null"`
-	ExpiresAt           time.Time `gorm:"default:current_timestamp"`
+	SessionID string    `gorm:"type:varchar(100);unique_index;not null"`
+	UserID    uint      `gorm:"not null"`
+	ExpiresAt time.Time `gorm:"not null"`
 }
