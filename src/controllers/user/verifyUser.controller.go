f58519ef94ab6a188d879297b1a873fa7ba49a97
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-09-28 14:07:06 +0100

    fix: Return an error if the user wasn't found

--- src/controllers/user/verifyUser.controller.go
+++ src/controllers/user/verifyUser.controller.go
@@ -1,19 +1,27 @@
 package controllers
 
-import "easy-wallet-be/src/models"
+import (
+	"easy-wallet-be/src/models"
+	"errors"
+)
 
 // VerifyUserByToken updates the user_verified field to true for the user with the given verify_user_token.
-// It takes a token string as input and returns an error if the update operation fails.
+// It returns an error if the update operation fails or if no user was updated.
 func VerifyUserByToken(token string) error {
 	db := models.DB()
 
-	err := db.Model(&models.User{}).
+	// Use the Update method and capture the result to check the number of affected rows.
+	result := db.Model(&models.User{}).
 		Where("verify_user_token = ?", token).
-		Update("user_verified", true).
-		Error
+		Update("user_verified", true)
 
-	if err != nil {
-		return err
+	if result.Error != nil {
+		return result.Error
+	}
+
+	// Check the number of affected rows to see if any user was updated.
+	if result.RowsAffected == 0 {
+		return errors.New("no user was updated")
 	}
 
 	return nil
