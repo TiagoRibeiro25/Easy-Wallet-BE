0cff6dbaf4463adb1ec5b0019db10cd4100892d8
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-10-07 17:48:50 +0100

    fix: Fixed wrong schema types

--- src/data/schemas/categories/getCategories/schema.go
+++ src/data/schemas/categories/getCategories/schema.go
@@ -3,8 +3,7 @@ package schemas
 type ResponseData struct {
 	ID        uint   `json:"id"`
 	Name      string `json:"name"`
-	IconID    string `json:"icon_id"`
-	UserID    bool   `json:"user_id"`
+	IconID    uint   `json:"icon_id"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
 }
--- src/models/category.model.go
+++ src/models/category.model.go
@@ -10,7 +10,7 @@ import (
 type Category struct {
 	gorm.Model
 	Name   string `gorm:"type:varchar(100);not null"`
-	IconID int    `gorm:"not null"`
+	IconID uint   `gorm:"not null"`
 	UserID uint   `gorm:"index;not null"`
 
 	Expenses []Expense `gorm:"foreignkey:CategoryID;constraint:OnDelete:CASCADE;"`
@@ -31,7 +31,7 @@ func AddDefaultCategories(userID uint) error {
 
 	var categories []struct {
 		Name   string `json:"name"`
-		IconID int    `json:"icon_id"`
+		IconID uint   `json:"icon_id"`
 	}
 
 	decoder := json.NewDecoder(file)
