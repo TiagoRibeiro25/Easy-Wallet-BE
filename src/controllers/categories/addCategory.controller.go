a0a724cee979c2554de39335ae7a82f2a96dfb03
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-10-11 11:35:48 +0100

    refactor: Removed useless console logging

--- src/controllers/categories/addCategory.controller.go
+++ src/controllers/categories/addCategory.controller.go
@@ -2,20 +2,15 @@ package controllers
 
 import (
 	"easy-wallet-be/src/models"
-	"fmt"
 )
 
 // AddCategory adds a new category to the database for the given user.
 // It takes the user ID, new category name and new category icon ID as input parameters.
 // It returns an error if there is any issue while creating the new category.
 func AddCategory(userID uint, newCategoryName string, newCategoryIconID uint) error {
-	fmt.Println("userID: ", userID)
-	fmt.Println("newCategoryName: ", newCategoryName)
-	fmt.Println("newCategoryIconID: ", newCategoryIconID)
-
 	db := models.DB()
 
-	// create new category (table: categories)
+	// Create new category
 	newCategory := models.Category{
 		Name:   newCategoryName,
 		IconID: newCategoryIconID,
