02735e63f705de2058970e94ce19886524d5409d
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-02-18 18:45:41 +0000

    feat: Added 2 util functions to validate data

---
+++ src/utils/validateData.util.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"time"
+)
+
+// Validates if the given string represents a valid date in the format "YYYY-MM-DD".
+// It returns true if the date is valid, otherwise it returns false.
+func ValidateDate(date string) bool {
+	_, err := time.Parse("2006-01-02", date)
+	return err == nil
+}
+
+// IsDateInFuture checks if the given date is in the future.
+// It takes a string representation of a date in the format "2006-01-02" and returns a boolean value.
+// The function returns true if the parsed date is after the current time, and false otherwise.
+func IsDateInFuture(date string) bool {
+	parsedDate, _ := time.Parse("2006-01-02", date)
+	return parsedDate.After(time.Now())
+}
