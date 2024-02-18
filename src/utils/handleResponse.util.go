67573008f08bd78affb1a945471f9763ffb1c701
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-02-18 18:45:31 +0000

    refactor: Reduced handleResponse code

--- src/utils/handleResponse.util.go
+++ src/utils/handleResponse.util.go
@@ -19,17 +19,15 @@ func HandleResponse(context echo.Context, statusCode int, message string, data i
 		return context.NoContent(statusCode)
 	}
 
-	response := Response{
-		Success: statusCode >= 200 && statusCode < 300,
-		Message: message,
-		Data:    data,
-	}
-
 	// Print the response message depending on the status code of the response.
 	if statusCode >= 500 {
 		color.New(color.FgHiYellow).Print("Server Error: ")
 		color.New(color.FgHiRed).Println(message)
 	}
 
-	return context.JSON(statusCode, response)
+	return context.JSON(statusCode, Response{
+		Success: statusCode >= 200 && statusCode < 300,
+		Message: message,
+		Data:    data,
+	})
 }
