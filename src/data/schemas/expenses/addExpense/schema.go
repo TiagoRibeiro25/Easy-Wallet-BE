d5ccab4a96f916c32cc3f3bea61e7045fa27b8d1
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2024-02-19 11:25:50 +0000

    fix: Added missing property to type

--- src/data/schemas/expenses/addExpense/schema.go
+++ src/data/schemas/expenses/addExpense/schema.go
@@ -17,4 +17,5 @@ type ResponseData struct {
 	Description string `json:"description"`
 	CreatedAt   string `json:"created_at"`
 	UpdatedAt   string `json:"updated_at"`
+	DeletedAt   string `json:"deleted_at"`
 }
